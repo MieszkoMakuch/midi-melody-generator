72db037ac8f13d7a1f1d80c2b275bf5260d47fef
Mieszko Makuch <mieszkomakuch@gmail.com>
2017-04-03 00:41:42 +0200

    Code refactor: moved parse_arguments() to new module arg_parser.py

---
+++ arg_parser.py
@@ -0,0 +1,36 @@
+import argparse
+
+from melody_properties import MelodyProperties
+
+
+def parse_arguments():
+    mood_choices = [x.name for x in MelodyProperties.Moods]
+    speed_choices = [x.name for x in MelodyProperties.Speeds]
+    instrument_name_choices = [x.name for x in MelodyProperties.Instruments]
+    instrument_value_choices = [x.value for x in MelodyProperties.Instruments]
+
+    parser = argparse.ArgumentParser(description="Generate narcotic MIDI melody")
+    parser.add_argument("-d", "--destination", dest="file_dest", metavar="", default=None,
+                        help="Choose MIDI file destination (default \"midi/melody.mid\")")
+    parser.add_argument("-m", "--mood", metavar="", choices=mood_choices, default=None,
+                        help="Choose mood of the melody")
+    parser.add_argument("-s", "--speed", metavar="", default=None,
+                        help="Choose speed of the melody", choices=speed_choices)
+
+    instrument = parser.add_mutually_exclusive_group()
+    instrument.add_argument("-in", "--instrumentName", metavar="", default=None,
+                            help="Choose name of the instrument", choices=instrument_name_choices)
+    instrument.add_argument("-iv", "--instrumentValue", metavar="", default=None, type=int,
+                            help="Choose value of the instrument", choices=instrument_value_choices)
+
+    args = parser.parse_args()
+
+    # check if file_dest has valid extension (.mid or .midi)
+    if args.file_dest and not args.file_dest.endswith(".mid") and not args.file_dest.endswith(".midi"):
+        args.file_dest += ".mid"
+
+    # if instrument was chosen by value assign its name
+    if args.instrumentValue is not None:
+        args.instrumentName = MelodyProperties.Instruments(args.instrumentValue).name
+
+    return args
