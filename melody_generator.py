69e7d690f7862f94b2db0bfcb68d615e9c36ce7f
Mieszko Makuch <mieszkomakuch@gmail.com>
2017-04-03 12:42:32 +0200

    Added docstrings to melody_generator. MelodyGenerator class- added ability to choose mod randomly (if mood parameter is not specified)

--- melody_generator.py
+++ melody_generator.py
@@ -7,11 +7,62 @@ from melody_properties import MelodyProperties
 from pyknon import genmidi
 from pyknon.music import NoteSeq
 
+""""Module responsible for melody generation (midi file generation)"""
+
+
+class MelodyGenerator:
+    """Class responsible for generating melody (midi file)."""
+
+    def __init__(self, mood=None, speed=None, instrument=None, file_dest=None):
+        """All parameters are optional, if parameter is not specified
+        it will be chosen randomly or by default"""
+
+        if mood is None:
+            self.mood = random.choice(list(MelodyProperties.Moods))
+        else:
+            self.mood = MelodyProperties.Moods[mood]
+
+        if speed is None:
+            self.speed = MelodyProperties.Speeds.MEDIUM
+        else:
+            self.speed = MelodyProperties.Speeds[speed]
+
+        if instrument is None:
+            self.instrument = random.choice([0, 3])
+        else:
+            self.instrument = MelodyProperties.Instruments[instrument]
+
+        if file_dest is None:
+            self.file_dest = "midi/melodyMG.mid"
+        else:
+            self.file_dest = file_dest
+
+    def generate_midi_melody(self):
+        """Generates melody based on given parameters"""
+
+        print("Mood: " + MelodyProperties.Moods(self.mood).name)
+        midi = genmidi.Midi(1, tempo=self.speed.value, instrument=self.instrument)
+        print("Instrument: " + MelodyProperties.Instruments(self.instrument).name)
+
+        octave_shift = -1
+        chord_seq = ChordSeq(mood=self.mood).octave_shift(octave_shift).__mul__(2)
+        print("Chord sequence: " + chord_seq.verbose)
+
+        right_hand_seq = RhythmGenerator(chord_seq, RhythmGenerator.RhythmTypes.RIGHT_HAND).generate_note_seq()
+        left_hand_seq = RhythmGenerator(chord_seq, RhythmGenerator.RhythmTypes.LEFT_HAND).generate_note_seq()
+
+        midi.seq_chords(right_hand_seq)
+        midi.seq_chords(left_hand_seq)
+        midi.write(self.file_dest)
+        print("Melody generated")
+
 
 class RhythmGenerator:
+    """Class responsible for generating rhythm based on the given ChordSeq."""
+
     class RhythmTypes(Enum):
-        LEFT = 1
-        RIGHT = 2
+        LEFT_HAND = 1
+        RIGHT_HAND = 2
 
     def __init__(self, chords, rhythm_type, subdivided=32):
         self.chords = chords
@@ -19,36 +70,31 @@ class RhythmGenerator:
         self.rhythms_right = [self.rhythm_4_40_right,
                               self.rhythm_6_17_right,
                               self.rhythm_heart_and_soul_right,
-                              self.rhythm_8th_note_pattern_right,
-                              self.rhythm_16th_note_pattern_right]
+                              self.rhythm_8th_note_subdivided_right,
+                              self.rhythm_16th_note_subdivided_right]
 
         self.rhythms_left = [self.rhythm_4_40_left,
                              self.rhythm_6_17_left,
                              self.rhythm_heart_and_soul_left,
                              self.rhythm_constant_chord_left]
 
-        if rhythm_type == self.RhythmTypes.LEFT:
+        if rhythm_type == self.RhythmTypes.LEFT_HAND:
             self.rhythm_pattern = random.choice(self.rhythms_left)
             print("Chosen rhythm: " + self.rhythm_pattern.__name__)
-        elif rhythm_type == self.RhythmTypes.RIGHT:
+        elif rhythm_type == self.RhythmTypes.RIGHT_HAND:
             self.rhythm_pattern = random.choice(self.rhythms_right)
             print("Chosen rhythm: " + self.rhythm_pattern.__name__)
         else:
             raise AttributeError("RhythmGenerator does not accept this rhythm_type")
 
     def generate_note_seq(self) -> [NoteSeq]:
+        """Generates note sequence based on given chord sequence and rhythm pattern"""
         note_seq = []
         for chord in self.chords:
             note_seq += self.rhythm_pattern(chord)
         return [chord for chord in note_seq]
 
-    def get_rhythm_pattern(self, left: bool):
-        if left:
-            rhythm = random.choice(self.rhythms_left)
-        else:
-            rhythm = random.choice(self.rhythms_right)
-        return rhythm
-
+    # Definitions of basic rhythms patterns
     def rhythm_4_40_left(self, chord: Chord) -> [Chord]:
         first_note = Chord([chord.first_note.octave_shift(-1)])
         left_hand_seq = []
@@ -126,7 +172,7 @@ class RhythmGenerator:
 
         return rhythm
 
-    def rhythm_16th_note_pattern_right(self, chord: Chord) -> [NoteSeq]:
+    def rhythm_16th_note_subdivided_right(self, chord: Chord) -> [NoteSeq]:
         # works best with subdivided = 20
         chord = chord.stretch_dur(2 / self.subdivided)
         l1 = [NoteSeq([chord[0]])] + [NoteSeq([chord[1]])]
@@ -138,7 +184,7 @@ class RhythmGenerator:
 
         return rhythm
 
-    def rhythm_8th_note_pattern_right(self, chord: Chord) -> [NoteSeq]:
+    def rhythm_8th_note_subdivided_right(self, chord: Chord) -> [NoteSeq]:
         rhythm = []
         for i in range(0, 4):
             rhythm += [chord.top().stretch_dur(4 / self.subdivided)] + [chord.bottom().stretch_dur(4 / self.subdivided)]
@@ -148,42 +194,3 @@ class RhythmGenerator:
         first_note = chord.first_note.stretch_dur(32 / self.subdivided)
         rhythm = [Chord([first_note.octave_shift(-1)] + [first_note.octave_shift(-2)])]
         return rhythm
-
-
-class MelodyGenerator:
-    def __init__(self, mood=None, speed=None, instrument=None, file_dest=None):
-        if mood is None:
-            self.mood = MelodyProperties.Moods.HAPPY
-        else:
-            self.mood = MelodyProperties.Moods[mood]
-
-        if speed is None:
-            self.speed = MelodyProperties.Speeds.MEDIUM
-        else:
-            self.speed = MelodyProperties.Speeds[speed]
-
-        if instrument is None:
-            self.instrument = random.choice([1, 3])
-        else:
-            self.instrument = MelodyProperties.Instruments[instrument]
-
-        if file_dest is None:
-            self.file_dest = "midi/melodyMG.mid"
-        else:
-            self.file_dest = file_dest
-
-    def generate_midi_melody(self):
-        midi = genmidi.Midi(1, tempo=self.speed.value, instrument=self.instrument)
-        print("Instrument: " + MelodyProperties.Instruments(self.instrument).name)
-
-        octave_shift = -1
-        chord_seq = ChordSeq(mood=self.mood).octave_shift(octave_shift).__mul__(2)
-        print("Chord sequence: " + chord_seq.verbose)
-
-        right_hand_seq = RhythmGenerator(chord_seq, RhythmGenerator.RhythmTypes.RIGHT).generate_note_seq()
-        left_hand_seq = RhythmGenerator(chord_seq, RhythmGenerator.RhythmTypes.LEFT).generate_note_seq()
-
-        midi.seq_chords(right_hand_seq)
-        midi.seq_chords(left_hand_seq)
-        midi.write(self.file_dest)
-        print("Melody generated")
